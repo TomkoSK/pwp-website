cc10eb3f224ae084b647df33411b5aa926ef0689
TomkoSK <tomaslukac599@gmail.com>
2024-05-01 00:09:46 +0200

    big work

--- public/script.js
+++ public/script.js
@@ -4,6 +4,44 @@ button.style.flexGrow = 0.1;
 button.style.height = "70%";
 var header = document.querySelector("#header");
 
+window.onload = function() {
+    document.body.style.webkitTransform = "scale(1)";
+    document.body.style.msTransform = "scale(1)";
+    document.body.style.transform = "scale(1)";
+};
+
+function makeHeader(){//Used to make the header of the webpage, same thing on every html file so its done through code
+    let header = document.createElement("div");
+    header.id = "header";
+    header.classList.add("fixedSize");
+    let span = document.createElement("span");
+    span.id = "logo";
+    span.innerHTML = "Patchwork Paintings";
+    header.appendChild(span);
+    let list = document.createElement("ul");
+    list.id = "pagelist";
+    let pageNames = ["Home", "Gallery", "Prices", "Commissions", "TOS", "About Us"];
+    let pageLinks = ["index.html", "gallery.html", "prices.html", "commissions.html", "tos.html", "aboutus.html"];
+    for(let i = 0; i < 6; i++){
+        let item = document.createElement("li");
+        let link = document.createElement("a");
+        link.setAttribute("href", pageLinks[i]);
+        link.innerHTML = pageNames[i];
+        item.appendChild(link);
+        list.appendChild(item);
+    }
+    header.appendChild(list);
+    document.body.insertBefore(header, document.body.firstChild);
+}//NO LONGER USED JUST KEPT HERE
+
+function fixSizes(){
+    let zoom = window.devicePixelRatio;
+    let nodes = document.querySelectorAll(".fixedSize")
+    console.log(window.innerWidth);
+    nodes.forEach((node) => {node.style.width = node.clientWidth*zoom+"px"});
+}
+
+fixSizes();
 
 function resizeButton(event){
     console.log(window.innerWidth, window.innerHeight);
